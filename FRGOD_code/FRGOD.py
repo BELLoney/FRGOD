be93d0ba547df616869969f54d44350811b6a693
Zhong Yuan <46039709+BElloney@users.noreply.github.com>
2023-07-24 21:35:02 +0800

    Update FRGOD.py

--- FRGOD_code/FRGOD.py
+++ FRGOD_code/FRGOD.py
@@ -1,22 +1,21 @@
-# Detecting Fuzzy Rough Granules-based Outlier Detection (FRGOD) algorithm
-# Please refer to the following papers:
-# Yuan Zhong, Chen Hongmei, Li Tianrui, Sang Binbin, Shu Wang.
-# Outlier Detection based on fuzzy rough granules in mixed attribute data,
-# IEEE Transactions on Cybernetics,2022.
-# Uploaded by Yuan Zhong on Mar. 30, 2023. E-mail:yuanzhong2799@foxmail.com.
+## Detecting Fuzzy Rough Granules-based Outlier Detection (FRGOD) algorithm
+## Please refer to the following papers:
+## Yuan Zhong, Chen Hongmei, Li Tianrui, Sang Binbin, Shu Wang.
+## Outlier Detection based on fuzzy rough granules in mixed attribute data,
+## IEEE Transactions on Cybernetics,2022.
+## Uploaded by Yuan Zhong on Mar. 30, 2023. E-mail:yuanzhong2799@foxmail.com.
 import numpy as np
 from scipy.io import loadmat
 from scipy.spatial.distance import cdist
 from sklearn.preprocessing import MinMaxScaler
 
-
-# input:
+def FRGOD(data, lammda):
+## input:
 # data is data matrix without decisions, where rows for samples and columns for attributes.
 # lammda is used to adjust the adaptive fuzzy radius.
 # Numeric attributes are normalized to [0,1].
-# output:
+## output:
 # Fuzzy rough granules-based outlier factor (FRGOF)
-def FRGOD(data, lammda):
     n, m = data.shape
 
     delta = np.zeros(m)
